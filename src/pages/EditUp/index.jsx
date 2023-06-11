017198cf9d65948f4836095702c266c11b66a215
lee_ay <lee_ay@163.com>
2023-06-11 10:14:34 +0000

    style: edit component

--- src/pages/EditUp/index.jsx
+++ src/pages/EditUp/index.jsx
@@ -5,7 +5,8 @@ import { Button,
         Result,
         Form,
         Grid,
-        Select  } from '@arco-design/web-react';
+        Select,
+        Card  } from '@arco-design/web-react';
         
 import { IconCheck,IconUndo } from '@arco-design/web-react/icon';
         
@@ -82,63 +83,66 @@ export default function EditUp() {
 
   return (
     <div>
-        <Row className='grid-gutter-demo' gutter={24} style={{width:'1200px'}}>
-        <Col span={24}>
-        {
-            success ? 
-            (
-                <Result
-                    status='success'
-                    title='发布成功！'
-                    subTitle='内容已经成功发布~'
-                    extra={[
-                    <Button key='back' type='primary' onClick={()=>navigate('/index/recommend')}>
-                        去查看
-                    </Button>,
-                    ]}
-                ></Result>
-            )
-            :
-            (
-                    <Form layout='vertical' form={form} size='large' onValuesChange={(v, vs) => {
-                        console.log(v, vs);
-                    }}
-                    onSubmit={(v) => {
-                        submitArticle();
-                        console.log(v);
-                    }}>
-                        <FormItem label='标题' field='title' rules={[{ required: true,message: '标题不能为空哦~' }]}>
-                            <Input ref={inputRef} allowClear style={{marginRight: '10px'}}    onChange={inputTitle} placeholder='标题' maxLength={20} showWordLimit />
-                        </FormItem>
-                        <FormItem label='描述' field='describe' rules={[{ required: true,message: '描述不能为空哦~' }]}>
-                            <Input  allowClear style={{marginRight: '10px'}} onChange={inputTitle} placeholder='简单介绍一下' maxLength={40} showWordLimit />
-                        </FormItem>
-                        <FormItem label='标签' field='tags' rules={[{ required: true,message: '标签不能为空哦~' }]}>
-                            <Select mode={'multiple'} size={'large'} placeholder='选择标签' showSearch style={{ width: 345 }} >
-                                {options.map((option) => (
-                                <Option key={option} value={option}>
-                                    {option}
-                                </Option>
-                                ))}
-                            </Select>
-                        </FormItem>
-                        <FormItem label='内容' field='content' >
-                            <div id="vditor" style={{height: '100%'}}/>
-                        </FormItem>
-                        <FormItem wrapperCol={{ offset: 10 }}>
-                            <Space size="large">
-                                <Button type='primary' htmlType='submit' icon={<IconCheck />} loading={completeLoading}>发布</Button>
-                                <Button type='outline' icon={<IconUndo />} onClick={() => {form.resetFields();}}>重置</Button>
-                            </Space>
-                        </FormItem>
+        <Card>
+            <Row className='grid-gutter-demo' gutter={24} style={{width:'1200px'}}>
+                <Col span={24}>
+                {
+                    success ? 
+                    (
+                        <Result
+                            status='success'
+                            title='发布成功！'
+                            subTitle='内容已经成功发布~'
+                            extra={[
+                            <Button key='back' type='primary' onClick={()=>navigate('/index/recommend')}>
+                                去查看
+                            </Button>,
+                            ]}
+                        ></Result>
+                    )
+                    :
+                    (
+                            <Form layout='vertical' form={form} size='large' onValuesChange={(v, vs) => {
+                                console.log(v, vs);
+                            }}
+                            onSubmit={(v) => {
+                                submitArticle();
+                                console.log(v);
+                            }}>
+                                <FormItem label='标题' field='title' rules={[{ required: true,message: '标题不能为空哦~' }]}>
+                                    <Input ref={inputRef} allowClear style={{marginRight: '10px'}}    onChange={inputTitle} placeholder='标题' maxLength={20} showWordLimit />
+                                </FormItem>
+                                <FormItem label='描述' field='describe' rules={[{ required: true,message: '描述不能为空哦~' }]}>
+                                    <Input  allowClear style={{marginRight: '10px'}} onChange={inputTitle} placeholder='简单介绍一下' maxLength={40} showWordLimit />
+                                </FormItem>
+                                <FormItem label='标签' field='tags' rules={[{ required: true,message: '标签不能为空哦~' }]}>
+                                    <Select mode={'multiple'} size={'large'} placeholder='选择标签' showSearch style={{ width: 345 }} >
+                                        {options.map((option) => (
+                                        <Option key={option} value={option}>
+                                            {option}
+                                        </Option>
+                                        ))}
+                                    </Select>
+                                </FormItem>
+                                <FormItem label={<div>内容 <span style={{fontSize: '12px',color: '#7d8186'}}>（全屏编辑体验更佳）</span></div>} field='content' >
+                                    <div id="vditor" style={{height: '100%'}}/>
+                                </FormItem>
+                                <FormItem wrapperCol={{ offset: 10 }}>
+                                    <Space size="large">
+                                        <Button type='primary' htmlType='submit' icon={<IconCheck />} loading={completeLoading}>发布</Button>
+                                        <Button type='outline' icon={<IconUndo />} onClick={() => {form.resetFields();}}>重置</Button>
+                                    </Space>
+                                </FormItem>
 
-                    </Form>
-            )
-        }
-        </Col>
-        {/* <Col span={2}></Col> */}
+                            </Form>
+                    )
+                }
+                </Col>
+                {/* <Col span={2}></Col> */}
+                
+            </Row>
+        </Card>
         
-      </Row>
     </div>
 
   )
